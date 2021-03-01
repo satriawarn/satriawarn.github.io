6222100e28da75c1d82b3b9e37c9aff5275dfb95
Erik Satriawan <erik.satriawan@incenplus.com>
2021-03-01 14:47:09 +0700

    github_page

---
+++ js/load.js
@@ -0,0 +1,53 @@
+/*
+ *
+ * akshay sharma 
+ * fxn769@gmail.com
+ * 5/7/2020
+ * 
+ */
+
+var socialtags = [{
+    img: "img/github.svg",
+    link: "https://github.com/satriawarn"
+}, {
+    img: "img/linkedin.svg",
+    link: "https://www.linkedin.com/in/eriksatriawan/"
+}, {
+    img: "img/twitter.svg",
+    link: "https://twitter.com/satriawarn"
+}]
+
+function loadSocialIcons() {
+    var socialtext = ""
+    for (x in socialtags) {
+        var data = `<a class="center" href="` + socialtags[x].link + `"> <img class="center" style="padding: 5px!important;width: 40px;" src="` + socialtags[x].img + `"></a>`
+        socialtext += data
+    }
+    document.getElementById("social-list").innerHTML = socialtext;
+}
+
+loadSocialIcons()
+
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
+
+checkDayNight()
